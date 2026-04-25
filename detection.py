2b7348d46788249ccb9a1105de9d510897332549
mbarbier <michael.barbier@gmail.com>
2026-04-25 11:48:37 +0300

    visdrone training

--- detection.py
+++ detection.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 # Load a model
 model = YOLO("yolo26n.pt")  # load pretrained model
 
-results = model.train(data="data_det_light.yaml", epochs=3, imgsz=640)
+results = model.train(data="data_det_light.yaml", epochs=100, imgsz=640)
