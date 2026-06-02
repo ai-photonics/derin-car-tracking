f5cce3ee2deeeb4f4b80d83c89fa67e9fdf22a50
Michael <mbarbier@debian.iyte.edu.tr>
2026-06-02 01:30:07 -0400

    train-18: dark50 light50

--- detection.py
+++ detection.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo26l.pt")  # load pretrained model
 #model = YOLO("runs/detect/train-9/weights/best.pt")  # load pretrained model
 
-results = model.train(data="data_det_light100_dark100.yaml", epochs=300, rect=True, patience=0) #imgsz=640)
+results = model.train(data="data_det_light50_dark50.yaml", epochs=300, rect=True, patience=0) #imgsz=640)
